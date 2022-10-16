e2841e0393df208c6f0a836cd37595217017e32f
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2022-10-16 14:48:40 +0500

    add support send button

--- lib/pages/support_page/supoort_page.dart
+++ lib/pages/support_page/supoort_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../widgets/core/custom_elevated_button.dart';
 import '../../widgets/core/custom_textformfield.dart';
 import '../footer/footer.dart';
+import '../landing_page/landing_page.dart';
 
 class SupportPage extends StatelessWidget {
   const SupportPage({Key? key}) : super(key: key);
@@ -16,36 +19,45 @@ class SupportPage extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
-                  SizedBox(height: 30),
-                  Center(
+                children: <Widget>[
+                  const SizedBox(height: 30),
+                  const Center(
                     child: Text(
                       'Sellout Support Center',
                       style:
                           TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
                     ),
                   ),
-                  SizedBox(height: 30),
-                  Text(
+                  const SizedBox(height: 30),
+                  const Text(
                     'Name',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                   ),
-                  CustomTextFormField(hint: 'Enter your name'),
-                  Text(
+                  const CustomTextFormField(hint: 'Enter your name'),
+                  const Text(
                     'Email',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                   ),
-                  CustomTextFormField(hint: 'Enter your email'),
-                  Text(
+                  const CustomTextFormField(hint: 'Enter your email'),
+                  const Text(
                     'Subject',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                   ),
-                  CustomTextFormField(hint: 'Enter your subject'),
-                  Text(
+                  const CustomTextFormField(hint: 'Enter your subject'),
+                  const Text(
                     'Message',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                   ),
-                  CustomTextFormField(hint: 'Enter your message'),
+                  const CustomTextFormField(
+                      hint: 'Enter your message', maxLines: 5),
+                  SizedBox(
+                    width: 100,
+                    child: CustomElevatedButton(
+                      title: 'Send',
+                      onTap: () =>
+                          GoRouter.of(context).go(LandingPage.routePath),
+                    ),
+                  )
                 ],
               ),
             ),
